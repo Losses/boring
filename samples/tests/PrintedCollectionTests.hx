9024bd2aa23720dbe404731a8bba747a029d85fa
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-08-31 17:11:59 -0400

    test(std): preserve cross-target printed expectations

--- samples/tests/PrintedCollectionTests.hx
+++ samples/tests/PrintedCollectionTests.hx
@@ -10,6 +10,12 @@ class PrintedCollectionTests {
 	public static function collections():Void {
 		final v = new PrintedCollection(["alpha", "beta"], [1, 2], [new PrintedPoint(1, 2)], [[1, 2], [3]], []);
 		final expected = "PrintedCollection(names=[alpha, beta], counts=[1, 2], points=[PrintedPoint(x=1, y=2)], matrix=[[1, 2], [3]], none=[])";
-		Test.equals(RecordStr.str(v), v.toString());
+		#if boring_oracle
+		Test.equals(expected, v.toString());
+		Test.equals(expected, RecordStr.str(v));
+		#else
+		Test.equals(expected, v.toString());
+		Test.equals(expected, RecordStr.str(v));
+		#end
 	}
 }
