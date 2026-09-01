0a9a0262286d2b87f5e46ec88cf0948dffb72c51
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-01 19:56:27 -0400

    feat(bytes): expose copied subrange operation

--- src/reflaxe/rust/std-shadow/haxe/io/Bytes.hx
+++ src/reflaxe/rust/std-shadow/haxe/io/Bytes.hx
@@ -6,5 +6,6 @@ extern class Bytes {
 	function get(index:Int):Int;
 	function set(index:Int, value:Int):Void;
 	function blit(pos:Int, src:Bytes, srcPos:Int, length:Int):Void;
+	function sub(pos:Int, length:Int):Bytes;
 	function fill(pos:Int, length:Int, value:Int):Void;
 }
