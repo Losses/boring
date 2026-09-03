8a9113ee3e6647e9af2cbf9ac5e43a7fc65ae056
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-02 20:29:19 -0400

    fix(16): filter Rust computed key properties

--- src/reflaxe/rust/rustcompiler/RustType.hx
+++ src/reflaxe/rust/rustcompiler/RustType.hx
@@ -253,7 +253,7 @@ class RustType {
 		}
 		switch(Context.follow(field.type)) {
 			case TInst(c, _) if(c.get().meta.has(":dataClass")):
-				for(f in c.get().fields.get()) validateDataClassField(c.get(), f);
+				for(f in c.get().fields.get()) if(switch(f.kind) { case FVar(read, write): !(read.match(AccCall) && write.match(AccNever)); case _: false; }) validateDataClassField(c.get(), f);
 			case _:
 		}
 	}
