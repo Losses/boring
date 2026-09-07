ca8daa124524a975ef2f84e716a5877b98187b54
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-07 14:56:47 -0400

    fix(rust): bound generated string formatting

--- packages/compiler/reflaxe/rust/rustcompiler/RustDecl.hx
+++ packages/compiler/reflaxe/rust/rustcompiler/RustDecl.hx
@@ -162,7 +162,7 @@ class RustDecl {
         }
         final staticFunctionLines:Array<String> = [];
         for (v in varFields) {
-            for (l in staticFunctionDecl(v))
+            for (l in staticFunctionDecl(cls, v))
                 staticFunctionLines.push(l);
         }
         final prefixLines = tableLines.concat(moduleStaticLines).concat(staticFunctionLines);
@@ -1017,7 +1017,7 @@ class RustDecl {
         return v.isStatic && isFunctionType(v.field.type);
     }
 
-    function staticFunctionDecl(v:ClassVarData):Array<String> {
+    function staticFunctionDecl(cls:ClassType, v:ClassVarData):Array<String> {
         if (!isStaticFunctionField(v)) {
             return [];
         }
@@ -1035,8 +1035,7 @@ class RustDecl {
         }
         // @:allow members use crate visibility so allowed cross-module references compile.
         final vis = field.isPublic ? "pub " : (field.meta.has(":allow") ? "pub(crate) " : "");
-        final name = RustImports.toScreamingSnakeCase(field.name);
-        final initializerText = expr.rawFunctionInitializer(initializer);
+        final name = RustImports.toScreamingSnakeCase(cls.name + "_" + field.name);
         return [
             vis + "static " + name + ": " + types.staticFunctionOf(field.type) + " = " + initializerText + ";"
         ];
