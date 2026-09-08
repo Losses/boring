92553e26ba8da4e9f702bc02213e17129746d323
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-08 13:00:40 -0400

    test(kotlin): add underscore parameter sample

---
+++ samples/boring/UnderscoreParamOps.hx
@@ -0,0 +1,21 @@
+package boring;
+
+class UnderscoreParamOps {
+    #if kotlin
+    public static function resolve(_:Int):Int {
+        return _ + 1;
+    }
+
+    public static function pair(_:Int, __:Int):Int {
+        return _ + __;
+    }
+    #else
+    public static function resolve(value:Int):Int {
+        return value + 1;
+    }
+
+    public static function pair(first:Int, second:Int):Int {
+        return first + second;
+    }
+    #end
+}
