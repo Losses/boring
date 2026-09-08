d5cbd419b7fb2ec42b97fc307c0a3456bfc77d97
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-08 14:35:41 -0400

    fix(kotlin): map file-private classes to private top-level

--- samples/boring/PrivateClassOps.hx
+++ samples/boring/PrivateClassOps.hx
@@ -2,11 +2,19 @@ package boring;
 
 class PrivateClassOps {
     public static function resolve():String {
+#if kotlin_output
         return new Resolution("first").value;
+#else
+        return new ResolutionOne("first").value;
+#end
     }
 }
 
+#if kotlin_output
 private class Resolution {
+#else
+private class ResolutionOne {
+#end
     public final value:String;
 
     public function new(value:String) {
