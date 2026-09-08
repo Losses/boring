e787e5843a28c8169ac664e81e739fec74082f98
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-08 16:52:15 -0400

    test(kotlin): reproduce widening outside call arguments

---
+++ samples/tests/NumericWidenTests.hx
@@ -0,0 +1,24 @@
+package tests;
+
+import boring.NumericWidenOps;
+import std.Test;
+
+class NumericWidenTests {
+    @:test("Int operands widen for Float arithmetic")
+    public static function testOperands():Void {
+        Test.equals(3.5, NumericWidenOps.operandFloat());
+        Test.equals(3.5, NumericWidenOps.operandDouble());
+    }
+
+    @:test("Int returns widen for Float results")
+    public static function testReturns():Void {
+        Test.equals(2.0, NumericWidenOps.returnFloat());
+        Test.equals(2.0, NumericWidenOps.returnDouble());
+    }
+
+    @:test("Int assignments widen for Float locals")
+    public static function testAssignments():Void {
+        Test.equals(2.0, NumericWidenOps.assignFloat());
+        Test.equals(2.0, NumericWidenOps.assignDouble());
+    }
+}
