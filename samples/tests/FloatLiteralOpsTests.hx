6fb881f4179780fc148eee4b1feaeb5e79d81017
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-09 07:10:24 -0400

    test(rust): keep target probes in existing cases

--- samples/tests/FloatLiteralOpsTests.hx
+++ samples/tests/FloatLiteralOpsTests.hx
@@ -4,16 +4,12 @@ import boring.FloatLiteralOps;
 import std.Test;
 
 class FloatLiteralOpsTests {
-    #if rust_output
-    @:test("Rust widens integer literals in Float constructor slots")
-    public static function rustWidenIntegerConstructorArgument():Void {
-        Test.equals(0.0, FloatLiteralOps.integerConstructorArgument());
-    }
-    #end
-
     @:test("leading-dot float in argument position lowers")
     public static function testArgLeadingDot():Void {
         Test.equals(0.001, FloatLiteralOps.argLeadingDot());
+        #if rust_output
+        Test.equals(0.0, FloatLiteralOps.integerConstructorArgument());
+        #end
     }
 
     @:test("negative leading-dot float in initializer position lowers")
