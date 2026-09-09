b1cbc410adf4232d18baeee6fa0e425810e66fc9
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-09 01:16:22 -0400

    test(rust): reproduce message-only exception throw stop

---
+++ samples/tests/HaxeExceptionTests.hx
@@ -0,0 +1,26 @@
+package tests;
+
+import std.Test;
+
+#if rust_output
+import boring.HaxeExceptionOps.HaxeExceptionFault;
+import boring.HaxeExceptionOps;
+#end
+
+class HaxeExceptionTests {
+    @:test("haxe.Exception subclasses preserve message values when thrown and caught")
+    public static function thrownMessage():Void {
+        #if rust_output
+        var message = "haxe exception message";
+        var caught = "";
+        try {
+            HaxeExceptionOps.throwFault(message);
+        } catch (error:HaxeExceptionFault) {
+            caught = error.message;
+        }
+        Test.equals(message, caught);
+        #else
+        Test.equals(true, true);
+        #end
+    }
+}
