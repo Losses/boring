4b2aac8acbb426a02e973c227b352042736fc8fa
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-13 03:53:23 -0400

    fix(swift): drop unreachable duplicate optionalExpr case

--- packages/compiler/reflaxe/swift/swiftcompiler/SwiftExpr.hx
+++ packages/compiler/reflaxe/swift/swiftcompiler/SwiftExpr.hx
@@ -2061,15 +2061,6 @@ class SwiftExpr {
                         case _: "(" + text + ")!";
                     }
                 };
-            case _: {
-                final text = expr(a);
-                if (!optionalValued(a) || StringTools.endsWith(text, "!"))
-                    text;
-                else switch (stripWrap(a).expr) {
-                    case TLocal(_): text + "!";
-                    case _: "(" + text + ")!";
-                }
-            };
         };
     }
 
