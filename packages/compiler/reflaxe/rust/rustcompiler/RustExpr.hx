349de57bc711dfa700cd23033ac51a5e07470b45
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-15 17:24:23 -0400

    debug: trace nullableStringViewArg

--- packages/compiler/reflaxe/rust/rustcompiler/RustExpr.hx
+++ packages/compiler/reflaxe/rust/rustcompiler/RustExpr.hx
@@ -1550,6 +1550,7 @@ class RustExpr {
         stays a str view.
     **/
     function nullableStringViewArg(arg:TypedExpr):Bool {
+        trace("nullableStringViewArg: " + expr(arg) + " isNull=" + isNullType(arg.t) + " collapsed=" + isNullableCollapsedLocal(arg));
         return isNullType(arg.t) && !isNullableCollapsedLocal(arg);
     }
 
