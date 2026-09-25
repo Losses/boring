5ba7835b0d1acd128e390b3f21a77a6307c3f146
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-25 18:22:18 -0400

    Revert "fix(rust): exclude UString::from from std String detection heuristic"
    This reverts commit f93cca8c82e463dab20f100fad3ea3a3b671f802.

--- packages/compiler/reflaxe/rust/rustcompiler/RustExpr.hx
+++ packages/compiler/reflaxe/rust/rustcompiler/RustExpr.hx
@@ -2201,7 +2201,7 @@ class RustExpr {
             case TConst(TString(_)): "&(" + rendered + ")";
             case TLocal(v) if (isBorrowedParamLocal(v)): rendered;
             case _:
-                if (!StringTools.startsWith(rendered, "UString::from(") && (StringTools.contains(rendered, ".to_string()") || StringTools.contains(rendered, "String::new()")
+                if (StringTools.contains(rendered, ".to_string()") || StringTools.contains(rendered, "String::new()")
                     || StringTools.startsWith(rendered, "format!") || StringTools.endsWith(rendered, ".name())"))
                     "&UString::from((" + rendered + ").as_str())";
                 else
@@ -12308,7 +12308,7 @@ class RustExpr {
                         case TConst(TString(_)): "&(" + argStr + ")";
                         case _ if (nullableStringViewArg(arg)): "(" + expr(arg) + ").as_deref().unwrap_or(\"\")";
                         case TLocal(v) if (isBorrowedParamLocal(v)): argStr;
-                        case _: if (!StringTools.startsWith(argStr, "UString::from(") && (StringTools.contains(argStr, ".to_string()") || StringTools.contains(argStr, "String::new()") || StringTools.startsWith(argStr, "format!") || StringTools.endsWith(argStr, ".name())")) "&UString::from((" + argStr + ").as_str())" else argStr + ".as_ustr()";
+                        case _: if (StringTools.contains(argStr, ".to_string()") || StringTools.contains(argStr, "String::new()") || StringTools.startsWith(argStr, "format!") || StringTools.endsWith(argStr, ".name())")) "&UString::from((" + argStr + ").as_str())" else argStr + ".as_ustr()";
                     });
                     continue;
                 }
